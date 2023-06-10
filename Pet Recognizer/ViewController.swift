90df2542ab6d11a17db632abfceae2f4a005e936
Giorgio Giannotta <jorge_upa@msn.com>
2023-06-10 06:57:16 +0200

    Push to GitHub

--- Pet Recognizer/ViewController.swift
+++ Pet Recognizer/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePickerPhoto = UIImagePickerController()
+    let imagePickerAlbum = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePickerPhoto.delegate = self
+        imagePickerAlbum.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {fatalError("couldn't convert uiimage to CIImage")}
+            
+            detect(image: ciimage)
+        }
+        
+        imagePickerPhoto.dismiss(animated: true, completion: nil)
+        
+        imagePickerAlbum.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: PetImageClassifier().model) else {fatalError("can't load ML model")}
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("unexpected result type from VNCoreMLRequest")}
+            
+            let topResult = results.first?.identifier
+            
+            self.navigationItem.title = topResult
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do { try handler.perform([request]) }
+        catch { print(error) }
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePickerPhoto.sourceType = .camera
+        imagePickerPhoto.allowsEditing = false
+        present(imagePickerPhoto, animated: true, completion: nil)
+    }
+    
+    @IBAction func albumTapped(_ sender: UIBarButtonItem) {
+        
+        imagePickerAlbum.sourceType = .photoLibrary
+        imagePickerAlbum.allowsEditing = false
+        present(imagePickerAlbum, animated: true, completion: nil)
+    }
 }
 
